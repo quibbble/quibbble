f2f72ac534b9766f33b2df3f753a4c2efc25ea45
Chris Fregly <dev@chrisfregly.com>
2022-10-16 22:19:08 -0700

    add discord banner

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
+import Discord from "./components/Discord";
 import Routing from "./components/Routing";
 import "./index.css";
 
-render(<Routing />, document.getElementById("root"))
+render(
+    <div>
+        <Discord />
+        <Routing />
+    </div>, document.getElementById("root"))
