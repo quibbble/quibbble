6674bc5e91aa75619321e93af5ffc2ef45b09646
Chris Fregly <dev@chrisfregly.com>
2022-10-16 22:29:25 -0700

    update discord banner

--- src/components/Discord.js
+++ src/components/Discord.js
@@ -1,6 +1,6 @@
 export default function Discord() {
     return (
-        <div className="font-bold top-0 flex justify-center w-full text-sm bg-violet-800 py-1">
+        <div className="font-bold top-0 flex justify-center text-center w-full text-sm bg-violet-800 p-4 md:p-2">
             <a href="https://discord.gg/VKvjutuhUp" target="_blank" rel="noreferrer">
                 <p>Looking for an opponent? Join our public <span className="text-yellow-300 italic">Discord</span> to find a game!</p>
             </a>
